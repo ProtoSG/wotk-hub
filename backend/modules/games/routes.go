ec1acd422a608a4a25687304155a027e895f7434
ProtoSG <protosg@users.noreply.github.com>
2026-08-02 23:19:41 -0500

    feat(games): add emoji movie game routes

---
+++ backend/modules/games/routes.go
@@ -0,0 +1,30 @@
+package games
+
+import (
+	"database/sql"
+	"net/http"
+
+	chi "github.com/go-chi/chi/v5"
+)
+
+type handler struct {
+	db *sql.DB
+}
+
+func Routes(db *sql.DB) http.Handler {
+	h := &handler{db: db}
+	r := chi.NewRouter()
+
+	r.Get("/emoji-movies", h.ListMovies)
+	r.Post("/emoji-movies", h.CreateMovie)
+	r.Get("/emoji-movies/random", h.RandomMovie)
+
+	r.Post("/sessions", h.CreateSession)
+	r.Get("/sessions/active", h.ActiveSessions)
+	r.Get("/sessions/{id}", h.GetSession)
+	r.Post("/sessions/{id}/join", h.JoinSession)
+	r.Post("/sessions/{id}/guess", h.Guess)
+	r.Post("/sessions/{id}/reveal", h.Reveal)
+
+	return r
+}
